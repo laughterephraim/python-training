fc68d52c1db3f240acee1ead6c3c50db9f5fc851
Laughter Ephraim <laughter.eo@gmail.com>
2026-05-28 09:15:36 +0100

    testing out functions by building an apply discount function

--- practice.py
+++ practice.py
@@ -1,1 +1,14 @@
-# testing jorrrr
+#Building an apply discount function
+def apply_discount(price, discount):
+    if not isinstance(price, (int, float)):
+        return('The price should be a number')
+    if not isinstance(discount, (int, float)):
+        return('The discount should be a number')
+    if price <= 0:
+        return('The price should be greater than 0')
+    if discount < 0 or discount > 100:
+        return('The discount should be between 0 and 100')
+    discounted_price = price * (1 - discount / 100)
+    return discounted_price
+#Testing the function
+print(apply_discount(100, 30))  # Expected output: 70.0
