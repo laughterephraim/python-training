5d6282a76a71b773e5478c26992e6489698bc880
Laughter Ephraim <laughter.eo@gmail.com>
2026-05-31 23:45:16 +0100

    Building an RPG character

--- selfpractice.py
+++ selfpractice.py
@@ -1,19 +1,27 @@
-#Building an RPG Character
-full_dot = '●'
-empty_dot = '○'
+# Building an RPG Character
+from os import name
 
-def create_character(name, strength, intelligence, charisma):
+
+full_dot = "●"
+empty_dot = "○"
+
+
+def create_character(character_name, strength, intelligence, charisma):
     # Validate the character's name
-    if not isinstance(name, str):
+    if not isinstance(character_name, str):
         return "The character name should be a string"
-    elif name == "":
+    elif character_name == "":
         return "The character should have a name"
-    elif len(name) > 10:
+    elif len(character_name) > 10:
         return "The character name is too long"
-    elif ' ' in name:
+    elif " " in character_name:
         return "The character name should not contain spaces"
     # Validate the character's stats
-    if not isinstance(strength, int) or not isinstance(intelligence, int) or not isinstance(charisma, int):
+    if (
+        not isinstance(strength, int)
+        or not isinstance(intelligence, int)
+        or not isinstance(charisma, int)
+    ):
         return "All stats should be integers"
     elif strength < 1 or intelligence < 1 or charisma < 1:
         return "All stats should be no less than 1"
@@ -22,7 +30,6 @@ def create_character(name, strength, intelligence, charisma):
     elif strength + intelligence + charisma != 7:
         return "The character should start with 7 points"
     # Building the output string
-    return f"""{name}
-    STR {full_dot * strength}{empty_dot * (10 - strength)}
-    INT {full_dot * intelligence}{empty_dot * (10 - intelligence)}
-    CHA {full_dot * charisma}{empty_dot * (10 - charisma)}"""
+    return f'{character_name}\nSTR {full_dot * strength}{empty_dot * (10 - strength)}\nINT {full_dot * intelligence}{empty_dot * (10 - intelligence)}\nCHA {full_dot * charisma}{empty_dot * (10 - charisma)}'
+
+print(create_character("ren", 4, 2, 1))
